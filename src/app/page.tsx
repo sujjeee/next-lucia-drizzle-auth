1b14fdf79da1580c41f79f2b00d6d7f6a85bdf01
Suraj <sujjeee@outlook.com>
2024-07-29 09:38:57 +0530

    redirect issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,27 +7,27 @@ import { redirect } from "next/navigation"
 import React from "react"
 
 export default async function Page() {
-  const { user } = await validateRequest()
-  if (!user) return redirect(" /login")
+  // const { user } = await validateRequest()
+  // if (!user) return redirect("/login")
 
   return (
     <div className=" flex justify-center items-center h-screen ">
       <div className=" flex flex-col justify-center items-center  h-full gap-5">
         <div>
           Hey
-          <a
+          {/* <a
             href={`mailto:${user.email}`}
             className="font-medium text-blue-500 hover:underline ml-1"
           >
             {user.email}
           </a>
-          !
+          ! */}
         </div>
-        <div className=" w-full">
+        {/* <div className=" w-full">
           <Button asChild>
             <Link href="/logout">Logout</Link>
           </Button>
-        </div>
+        </div> */}
       </div>
     </div>
   )
